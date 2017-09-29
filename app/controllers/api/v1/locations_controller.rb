a1c01ef9aab4376e8fd332c0390932bf3bfa3a64
joshlacey <williamjoshualacey@gmail.com>
2017-09-29 09:24:04 -0400

    added cities method

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -7,8 +7,18 @@ class Api::V1::LocationsController < ApplicationController
 
   def states
     @states = Location.group(:state).select(:state).pluck(:state)
-    render json: @states.as_json , status: 200
+    render json: @states.as_json, status: 200
   end
 
+  def cities
+    @cities = Location.group(:city).select(:city).pluck(:city)
+    render json: @cities.as_json, status: 200
+  end
+
+  def cities_by_state
+    #state = params[:state]
+    #@cities = Location.where(state: state).group(:city).select(:city).pluck(:city)
+    #render json: @cities.as_json, status: 200
+  end
 
 end
