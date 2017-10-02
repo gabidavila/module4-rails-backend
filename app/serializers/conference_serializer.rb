9ab3cd900e2ec00c945ba577440eeef98860c7e1
gabidavila <gabidavila@gmail.com>
2017-10-02 10:07:10 -0400

    added month to serializer

--- app/serializers/conference_serializer.rb
+++ app/serializers/conference_serializer.rb
@@ -1,5 +1,5 @@
 class ConferenceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :talks, :url, :start_date, :end_date, :address, :location, :organizer, :image_uri, :year
+  attributes :id, :name, :description, :talks, :url, :start_date, :end_date, :address, :location, :organizer, :image_uri, :year, :month
 
   def year
     object.start_date.year
